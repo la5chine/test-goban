ccaeb214a51a4ab413845e851fa54f434b7bc22d
la5chine <khachinechaker@gmail.com>
2023-07-27 11:55:57 +0100

    add a test case for checking if an empty position is taken or not

--- test_goban.py
+++ test_goban.py
@@ -59,3 +59,17 @@ def test_square_shape_is_taken():
     assert goban.is_taken(0, 2) is True
     assert goban.is_taken(1, 1) is True
     assert goban.is_taken(1, 2) is True
+
+
+def test_whether_an_empty_position_is_taken():
+    goban = Goban([
+        '...',
+        '##o',
+        '##o',
+        'oo.',
+    ])
+
+    assert goban.is_taken(0, 0) is False
+    assert goban.is_taken(1, 0) is False
+    assert goban.is_taken(2, 0) is False
+    assert goban.is_taken(2, 3) is False
