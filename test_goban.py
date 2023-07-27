be7db8daa6076416b27db82504070055e520e3a1
la5chine <khachinechaker@gmail.com>
2023-07-27 12:13:14 +0100

    format test file

--- test_goban.py
+++ test_goban.py
@@ -2,32 +2,38 @@ from goban import Goban
 
 
 def test_white_is_taken_when_surrounded_by_black():
-    goban = Goban([
-        '.#.',
-        '#o#',
-        '.#.',
-    ])
+    goban = Goban(
+        [
+            ".#.",
+            "#o#",
+            ".#.",
+        ]
+    )
 
     assert goban.is_taken(1, 1) is True
 
 
 def test_white_is_not_taken_when_it_has_a_liberty():
-    goban = Goban([
-        '...',
-        '#o#',
-        '.#.',
-    ])
+    goban = Goban(
+        [
+            "...",
+            "#o#",
+            ".#.",
+        ]
+    )
 
     assert goban.is_taken(1, 1) is False
 
 
 def test_black_shape_is_taken_when_surrounded():
-    goban = Goban([
-        'oo.',
-        '##o',
-        'o#o',
-        '.o.',
-    ])
+    goban = Goban(
+        [
+            "oo.",
+            "##o",
+            "o#o",
+            ".o.",
+        ]
+    )
 
     assert goban.is_taken(0, 1) is True
     assert goban.is_taken(1, 1) is True
@@ -35,12 +41,14 @@ def test_black_shape_is_taken_when_surrounded():
 
 
 def test_black_shape_is_not_taken_when_it_has_a_liberty():
-    goban = Goban([
-        'oo.',
-        '##.',
-        'o#o',
-        '.o.',
-    ])
+    goban = Goban(
+        [
+            "oo.",
+            "##.",
+            "o#o",
+            ".o.",
+        ]
+    )
 
     assert goban.is_taken(0, 1) is False
     assert goban.is_taken(1, 1) is False
@@ -48,12 +56,14 @@ def test_black_shape_is_not_taken_when_it_has_a_liberty():
 
 
 def test_square_shape_is_taken():
-    goban = Goban([
-        'oo.',
-        '##o',
-        '##o',
-        'oo.',
-    ])
+    goban = Goban(
+        [
+            "oo.",
+            "##o",
+            "##o",
+            "oo.",
+        ]
+    )
 
     assert goban.is_taken(0, 1) is True
     assert goban.is_taken(0, 2) is True
@@ -62,12 +72,14 @@ def test_square_shape_is_taken():
 
 
 def test_whether_an_empty_position_is_taken():
-    goban = Goban([
-        '...',
-        '##o',
-        '##o',
-        'oo.',
-    ])
+    goban = Goban(
+        [
+            "...",
+            "##o",
+            "##o",
+            "oo.",
+        ]
+    )
 
     assert goban.is_taken(0, 0) is False
     assert goban.is_taken(1, 0) is False
